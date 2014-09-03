5ddf44c57a595d2356b6f0ebd94065b26404660a
Chanakya <chanakyadevraj@gmail.com>
2014-09-03 13:32:27 +0530

    saimatha code

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Code::Application.config.secret_token = '21e22a1f7a068434ffb705a8402106ff29b7c761a7eef73dcdf57861632d276bcba68147ed61c0b4286ffb6d0ed0538394c936a8ad0febc1022d6f8a5724db45'
