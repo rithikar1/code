d02e2113bc37bdb4a11967713feb7f12449f43c9
rithikar1 <kunal_waghray@yahoo.co.in>
2014-09-18 19:09:57 +0530

    new changes

--- Gemfile
+++ Gemfile
@@ -1,6 +1,6 @@
 source 'https://rubygems.org'
 
-gem 'rails', '3.2.16'
+gem 'rails', '3.2.12'
 
 # Bundle edge Rails instead:
 # gem 'rails', :git => 'git://github.com/rails/rails.git'
@@ -24,6 +24,8 @@ gem "less-rails" #Sprockets (what Rails 3.1 uses for its asset pipeline) support
 gem "twitter-bootstrap-rails"
 
 gem 'jquery-rails'
+
+gem 'contact_us'
 
 # To use ActiveModel has_secure_password
 # gem 'bcrypt-ruby', '~> 3.0.0'
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -34,4 +34,17 @@ Code::Application.configure do
 
   # Expands the lines which load the assets
   config.assets.debug = true
+
+ config.action_mailer.raise_delivery_errors = true
+config.action_mailer.perform_deliveries = true
+config.action_mailer.delivery_method = :smtp
+config.action_mailer.smtp_settings = {
+:address => "smtp.mailgun.org",
+:port => 587,
+:domain => 'mailgun.com',
+:user_name => '   postmaster@sandbox94529.mailgun.org',
+:password => '12344321',
+:authentication => 'plain',
+:enable_starttls_auto => true
+}
 end
